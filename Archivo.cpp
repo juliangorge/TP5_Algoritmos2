f8abb9ea42b07bce38805bd03e1630e1541530fd
Julián Gorge <juliangorge@hotmail.com>
2020-07-23 13:12:43 -0300

    Archivo borrador

---
+++ Archivo.cpp
@@ -0,0 +1,112 @@
+#include "Archivo.h"
+
+#include "ABB.h"
+#include "Grafo.h"
+#include "Aeropuertos.h"
+#include "Vuelos.h"
+
+using namespace std;
+
+Archivo::Archivo(string nombre)
+{
+	this->nombre = nombre;
+}
+
+Archivo::~Archivo()
+{
+
+}
+
+void Archivo::abrirLectura()
+{
+	if(this->nombre == 'aeropuertos.txt'){
+		//Aeropuerto aeropuerto;
+		//aeropuerto.cargar();
+	}else if(this->nombre == 'vuelos.txt'){
+		//ArchivoVuelos vuelos;
+		//vuelos.cargar();
+	}else{
+		//cout << "No se pudo leer el archivo" << endl;
+	}
+
+}
+
+
+void ArchivoAeropuertos::cargar(){
+
+	ifstream archivo;
+	archivo.open(this->archivo);
+
+	string codigoIATA; //char codigoIATA[3];
+	string nombre;
+	string ciudad;
+	string pais;
+	float superficie;
+	int cantidadTerminales;
+	int destinosNacionales;
+	int destinosInternacionales;
+
+	while(!archivo.eof()){
+		getline(archivo, codigoIATA);
+		getline(archivo, nombre);
+		getline(archivo, ciudad);
+		getline(archivo, pais);
+		getline(archivo, superficie);
+		getline(archivo, cantidadTerminales);
+		getline(archivo, destinosNacionales);
+		getline(archivo, destinosInternacionales);
+
+		// Se crea el arbol y se insertan los datos del archivo
+    	BinarySearchTree<int>* arbol = new ABB <int>();
+
+    	Aeropuerto* aeropuerto = new Aeropuerto(codigoIATA, nombre, ciudad, pais, superficie, cantidadTerminales, destinosNacionales, destinosInternacionales);
+    	arbol->insertar(aeropuerto->getCodigo(), aeropuerto);
+	}
+
+	archivo.close();
+
+}
+
+void ArchivoVuelos::cargar(){
+
+	ifstream archivo;
+	archivo.open(this->archivo);
+
+	string codigoIATApartida; //char codigoIATApartida[3];
+	string codigoIATAdestino; //char codigoIATAdestino[3];
+	float costo;
+	float horas;
+
+	while(!archivo.eof()){
+		getline(archivo, codigoIATApartida);
+		getline(archivo, codigoIATAdestino);
+		getline(archivo, costo);
+		getline(archivo, horas);
+
+		// Se genera el grafo y se insertan los datos del archivo
+    	Vuelos* vuelo = new Vuelo(codigoIATApartida, codigoIATAdestino, costo, horas);
+
+		int indice_partida = grafo.getIndice(codigoIATApartida);
+		int indice_destino = grafo.getIndice(codigoIATAdestino);
+
+		grafo.agregarViaje(indice_partida, indice_destino, costo_vuelo);
+	}
+
+	archivo.close();
+
+}
+
+
+/*
+		if (indice_partida == -1) // Si no existe el aeropuerto
+			indice_partida = grafo.setVuelo(datos_aeropuerto_partida);
+		else
+			delete datos_aeropuerto_partida;
+		*/
+
+		/*
+		if (indice_destino == -1) // Si no existe el aeropuerto
+			indice_destino = grafo.setVuelo(datos_aeropuerto_destino);
+		else
+			delete datos_aeropuerto_destino;
+*/
---
+++ Archivo.h
@@ -0,0 +1,76 @@
+/*
+
+Leer archivo de Aeropuerto al iniciar la aplicación -> genera un diccionario ABB
+codigo_IATA nombre ciudad pais superficie #cantidad_terminales #destinos_nacionales #destinos_internacionales
+
+Consideraciones
+- El archivo está bien formado.
+- La implementación puede ser recursiva o iterativa.
+
+////
+
+Vuelos
+A partir de la lectura de un archivo de texto vuelos.txt, que deberá hacerse inmediatamente después de leer el archivo aeropuertos.txt, se generará un grafo pesado dirigido.
+En cada línea estará la información de cada vuelo, separada por espacios:
+codigo_IATA_partida código_IATA_destino costo_vuelo horas_vuelo
+
+Consideraciones
+- El archivo está bien formado.
+- La implementación puede ser recursiva o iterativa.
+
+
+Hacer una clase de lectura general que tenga 2 hijos uno para la lectura de los aeropuertos y otro para los vuelos.
+
+*/
+
+
+#ifndef ARCHIVO_H
+#define ARCHIVO_H
+
+#include <string>
+
+const string ARCHIVO_AEROPUERTOS = "aeropuertos.txt";
+const string ARCHIVO_VUELOS = "vuelos.txt";
+
+class Archivo
+{
+    private:
+        string nombre;
+
+    public:
+
+        //Constructor
+        Archivo(string nombre);
+
+        //Destructor
+        ~Archivo();
+
+        //PRE: -
+        //POST: 
+        void abrirLectura();
+
+        //PRE: -
+        //POST: 
+        virtual void cargar() = 0;
+
+};
+
+class ArchivoAeropuertos : public Archivo {
+
+    public:
+
+        // Instanciacion del metodo virtual puro de la clase Archivo
+        void cargar();
+
+};
+
+
+class ArchivoVuelos : public Archivo {
+
+    public:
+
+        // Instanciacion del metodo virtual puro de la clase Archivo
+        void cargar();
+
+}
+#endif // ARCHIVO_H
