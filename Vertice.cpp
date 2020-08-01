c424eeebe73b6462d9ed9abbcde3c0ae3e0eda78
tomasarrachea <tarrachea@fi.uba.ar>
2020-08-01 03:57:38 -0300

    Implementacion clase Vertice

--- Vertice.cpp
+++ Vertice.cpp
@@ -1,22 +1,50 @@
 #include"Vertice.h"
 #include<iostream>
-#include"Lista.h"
-#include"Vuelo.h"
 
 using namespace std;
 
-Vertice(){
-	iata = "";
+Vertice::Vertice(){
+	this->iata = "";
+	vuelos = Lista lista; 
 }
 
-~Vertice(){
+Vertice::~Vertice(){
 	vuelos.vaciarLista(true);
 }
 
-void agregarVuelo(Vuelo& vuelo){
-
+void Vertice::setIata(string iata){
+	this->iata = iata;
 }
 
-void borrarVuelo(string destino){
+void Vertice::agregarVuelo(Vuelo& vuelo){
+	vuelos.insertar(vuelo);
+}
 
+string Vertice::getIata(){
+	return iata;
+}
+
+Vuelo Vertice::getVuelo(string destino){
+	Vuelo aux;
+	for (int i = 1; i < vuelos.getTam(); ++i){
+		aux = vuelos.getDato(i);
+		if (aux.getDestino() == destino)
+			return aux;
+	}
+	return NULL;
+}
+
+bool Vertice::hayVuelo(string destino){
+	Vuelo aux;
+	for (int i = 1; i < vuelos.getTam(); ++i){
+		aux = vuelos.getDato(i);
+		if (aux.getDestino() == destino)
+			return true;
+	}
+	return false;
+}
+
+void Vertice::borrarVuelo(string destino){
+	vuelo = vuelos.getVuelo(destino);
+	vuelos.delDato(vuelo);
 }
--- Vertice.h
+++ Vertice.h
@@ -21,21 +21,29 @@ public:
 	// POST: destruye un objeto Vertice
 	~Vertice();
 
+	// PRE: recibe el codigo iata del vertice
+	// POST: actualiza el valor del codigo iata
+	void setIata(string iata);
+
+	// PRE: recibe un puntero a un vuelo creado
+	// POST: lo agrega a la lista del vertice
+	void agregarVuelo(Vuelo&);
+
 	// PRE:
 	// POST: devuelve el codigo iata del vertice
 	string getIata();
 
 	// PRE: recibe el codigo iata de un destino
 	// POST: devuelve un puntero al Vuelo correspondiente, si existe
-	Vuelo* getVuelo(string destino);
+	Vuelo getVuelo(string destino);
 
 	// PRE: recibe el codigo iata de un destino
 	// POST: devuelve true si existe un vuelo a ese destino
 	bool hayVuelo(string destino);
 
-	// PRE:
-	// POST: 
-	void agregarVuelo(Vuelo&);
+	// PRE: recibe el codigo iata de un destino
+	// POST: borra el vuelo correspondiente
+	void borrarVuelo(string destino);
 };
 
 #endif /* TP5_ALGORITMOS2_VERTICE_H_ */
