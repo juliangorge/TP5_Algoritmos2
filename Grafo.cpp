4a46febff185ca65bbc2eba1f9fff175b871155d
tomasarrachea <tarrachea@fi.uba.ar>
2020-07-31 23:09:09 -0300

    planteo inicial de Dijkstra

--- Grafo.cpp
+++ Grafo.cpp
@@ -1,5 +1,7 @@
 #include "Grafo.h"
 
+const float INFINITO = -1;
+
 Grafo::Grafo(){
 
 }
@@ -29,4 +31,42 @@ Vuelo caminoMasBarato(string partida, string destino){
 }
  
 Vuelo caminoMasCorto(string partida, string destino){
+	int n = vertices.getTam();
+	Vertice raiz = getVertice(partida); //funcion de grafo que busca en la lista segun el iata
+	int posRaiz = vertices.getPos(raiz); //posicion de la raiz en la lista vertices
+ 
+	float distancia [n];
+	bool visitado [n];
+
+	Vertice vertice; //variables auxiliares
+	Vuelo vuelo;
+
+	for (int i = 1; i < n; ++i){ //inicializa las etiquetas
+		vertice = vertices.getDato(i);
+		if (!raiz.hayVuelo( vertice.getIata() )){
+			distancia[i] = INFINITO;
+		} else {
+			vuelo = raiz.getVuelo( vertice.getIata() );
+			distancia[i] = vuelo.getHoras();
+		}
+		visto[i] = falso;
+	}
+	distancia[posRaiz] = 0;
+	visitado[posRaiz] = true; 
+	
+	Vertice actual;
+	int pos;
+	while (!todosVisitados(visitado)){ //recorre el vector y devuelve true si todas las posiciones son true
+		pos = minimo_distancia(distancia, visitado); //devuelve la posicion del vertice con menor distancia que no fue visitado
+		visitado[pos] = true;
+		actual = vertices.getDato(pos);
+		for (int i = 1; i < n; ++i){ //recorre toda la lista para ver cuales son adyacentes al actual
+			vertice = vertices.getDato(i);
+			if (actual.hayVuelo( vertice.getIata() )){
+				vuelo = actual.getVuelo( vertice.getIata() );
+				if (distancia[i] > distancia[pos] + vuelo.getHoras())
+					distancia[i] = distancia[pos] + vuelo.getHoras();
+			}
+		}
+	}
 }
